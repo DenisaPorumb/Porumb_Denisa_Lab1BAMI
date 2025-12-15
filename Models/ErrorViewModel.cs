81359d36e6bf6b74fdb2f47e9b0abc52c5073e19
DENISA-RALUCA PORUMB <denisa.porumb@stud.ubbcluj.ro>
2025-12-15 18:01:25 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Porumb_Denisa_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
